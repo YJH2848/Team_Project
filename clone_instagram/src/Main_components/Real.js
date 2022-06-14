5ada31df707f9f44c516f8f43ebe7a52b7e4c876
YJH2848 <yunj05763@gmail.com>
2022-06-14 20:33:50 +0900

    수정

--- clone_instagram/src/Main_components/Real.js
+++ clone_instagram/src/Main_components/Real.js
@@ -3,20 +3,10 @@ export default function real() {
     return (
         <Link to="/main" style={{ textDecoration: 'none', color: 'inherit', cursor: 'default' }}>
             <nav class="navbar navbar-light bg-light">
-                <div class="col-md-3"></div>    
-                <a href="http://localhost:3000/"><img width="100" height="40" src="./image/pngegg.png"></img></a>
-                <div class="container w-50 col-md-2">
-                    <div class="d-flex align-items-center col-md-6" className="search">
-                        <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" className="Magnifier"></img>
-                        <input class="form-control" type="search" placeholder="검색" aria-label="Search" />
-                    </div>
+                <img className="logo" width="110" height="40" src='./image/pngegg.png'></img>
+                <div className="search">
+                    <input class="form-control mr-sm-5" type="search" placeholder="검색" aria-label="Search" />
                 </div>
-                <div class="col-md-1"></div>
-                <a href="http://localhost:3000/"><i class="fa-solid fa-house fa-lg mr-4"></i></a>
-                <a href="#"><i class="fa-solid fa-paper-plane fa-lg mr-4"></i></a>
-                <a href="#"><i class="fa-regular fa-square-plus fa-lg mr-4"></i></a>
-                <a href="#"><i class="fa-regular fa-compass fa-lg mr-4"></i></a>
-                <a href="#"><i class="fa-regular fa-heart fa-lg"></i></a>
             </nav>
             <div className="Side">
                 SideBar(제발 신중하게 보고 진행하자 제발^^)
