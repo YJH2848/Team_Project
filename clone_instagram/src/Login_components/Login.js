471df744e39a6e870eeeee64ca4465ece07f2fbe
YJH2848 <yunj05763@gmail.com>
2022-06-16 08:19:22 +0900

    update

--- clone_instagram/src/Login_components/Login.js
+++ clone_instagram/src/Login_components/Login.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom"
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "../context/AuthProvider";
+import axios from '../api/axios';
+const LOGIN_URL = '/auth';
+
 const Login = () => {
+    const { setAuth } = useContext(AuthContext);
     const userRef = useRef();
     const errRef = useRef();
 
@@ -19,10 +24,38 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(user, pwd);
-        setUser('');
-        setPwd('');
-        setSuccess(true);
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            }
+            else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            }
+            else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            }
+            else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
     }
     return (
         <>
@@ -35,49 +68,49 @@ const Login = () => {
                     </p>
                 </section>
             ) : (
-            <div className="container">
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" :
-                        "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <img width="185" height="70" src="./image/pngegg.png"></img>
-                    <div className="login">
-                        <form onSubmit={handleSubmit}>
-                            <div className="name">
-                                <label htmlFor="username"></label>
-                                <input
-                                    type="text"
-                                    id="username"
-                                    ref={userRef}
-                                    autoComplete="off"
-                                    onChange={(e) => setUser(e.target.value)}
-                                    value={user}
-                                    required
-                                    placeholder="사용자 이름 또는 이메일"
-                                />
-                            </div>
-                            <div className="pwd">
-                                <label htmlFor="password"></label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    ref={userRef}
-                                    autoComplete="off"
-                                    onChange={(e) => setPwd(e.target.value)}
-                                    value={pwd}
-                                    required
-                                    placeholder="비밀번호"
-                                />
-                            </div>
-                            <button>Sign In</button>
-                        </form>
-                    </div>
-                    <p>
-                        Need an Account?<br />
-                        <span className="line">
-                            <a href="#">Sign Up"</a>
-                        </span>
-                    </p>
-                </section>
+                <div className="container">
+                    <section>
+                        <p ref={errRef} className={errMsg ? "errmsg" :
+                            "offscreen"} aria-live="assertive">{errMsg}</p>
+                        <img width="185" height="70" src="./image/pngegg.png"></img>
+                        <div className="login">
+                            <form onSubmit={handleSubmit}>
+                                <div className="name">
+                                    <label htmlFor="username"></label>
+                                    <input
+                                        type="text"
+                                        id="username"
+                                        ref={userRef}
+                                        autoComplete="off"
+                                        onChange={(e) => setUser(e.target.value)}
+                                        value={user}
+                                        required
+                                        placeholder="사용자 이름 또는 이메일"
+                                    />
+                                </div>
+                                <div className="pwd">
+                                    <label htmlFor="password"></label>
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        ref={userRef}
+                                        autoComplete="off"
+                                        onChange={(e) => setPwd(e.target.value)}
+                                        value={pwd}
+                                        required
+                                        placeholder="비밀번호"
+                                    />
+                                </div>
+                                <button>Sign In</button>
+                            </form>
+                        </div>
+                        <p>
+                            Need an Account?<br />
+                            <span className="line">
+                                <a href="#">Sign Up"</a>
+                            </span>
+                        </p>
+                    </section>
                 </div>
             )}
         </>
