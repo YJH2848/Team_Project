0158220908fd7d36526954c88f2e8036856159ec
YJH2848 <yunj05763@gmail.com>
2022-06-16 12:27:21 +0900

    last update

--- clone_instagram/src/App.js
+++ clone_instagram/src/App.js
@@ -3,18 +3,22 @@ import "./Main_components/Side/SideBar.css";
 import "./Main_components/Acti/Activity.css";
 import "./Main_components/post/Post.css";
 import "./Login_components/Login.css";
+import "./Sign_components/Sign.css";
+
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./Login_components/Login";
 import Real from "./Main_components/Real";
+import Sign from "./Sign_components/Sign";
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/main" element={<Real/>}></Route>
-        <Route path="/" element={<Login/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/main" element={<Real />}></Route>
+          <Route path="/sign" element={<Sign />}></Route>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   )
